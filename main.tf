56fcf6adb483f08f7250ab37d2f6e95f2a915637
sachindu00 <sachindumihiran44@gmail.com>
2025-08-29 01:49:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # You can change this to your preferred region
+}
+
+# Create a VPC module for networking
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true # Cost-saving for a demo
+
+  tags = {
+    Terraform   = "true"
+    Environment = "demo"
+  }
+}
+
+# Create the EKS cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "demo-eks-cluster"
+  cluster_version = "1.28"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  # Node Group - the worker nodes
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"] # A low-cost instance type for the demo
+    }
+  }
+
+  tags = {
+    Environment = "demo"
+    Terraform   = "true"
+  }
+}
